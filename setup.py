6f508970417bbb8cba178521942507c0247038aa
Behzad <akbardehghani1@gmail.com>
2019-02-12 23:44:59 -0500

    Add classification

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='paper-generalizability-window-size',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
